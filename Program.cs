0f2bcbfec3c1b9fc1c0f4c8e374e9571686e55cc
Bryan060705 <leowon67@gmail.com>
2025-10-16 18:22:11 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿global using Demo.Models;
+using Microsoft.AspNetCore.Http.Features;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddSqlServer<DB>($@"
+    Data Source=(LocalDB)\MSSQLLocalDB;
+    AttachDbFilename={builder.Environment.ContentRootPath}\DB.mdf;
+");
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = 209715200; // 200MB
+});
+
+// 添加Session支持
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+// 使用Session
+app.UseSession();
+
+app.MapDefaultControllerRoute();
+app.Run();
